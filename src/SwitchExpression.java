cc9681cb53df52589549907016b22106ac4c46a7
Paulo Sérgio <pauloqueiroz16@gmail.com>
2021-10-03 21:27:24 -0300

    Adicionando nova forma de se trabalhar com switch case de forma menos verbosa utilizando lambda expressions

---
+++ src/SwitchExpression.java
@@ -0,0 +1,35 @@
+
+public class SwitchExpression {
+
+	//Novidade Java 14 - Switch expressions
+	public static void main(String[] args) {
+		String option = "1";
+		switch (option) {
+		case "1":
+			System.out.println("opcao 1");
+			break;
+		case "2":
+			System.out.println("opcao 2");
+			break;
+		case "3":
+			System.out.println("opcao 3");
+			break;			
+		default:
+			System.out.println("opcao padrao");
+			break;
+		}
+		
+		/**
+		 * A partir do Java 14 eh possivel reescrever o switch case acima de forma menos
+		 * verbosa, utilzando lambda expressions, da seguinte forma:
+		 */
+		
+		switch (option) {
+		case "1" -> System.out.println("opcao 1");
+		case "2" -> System.out.println("opcao 2");
+		case "3" -> System.out.println("opcao 3");			
+		default -> System.out.println("opcao padrao");
+		}
+		
+	}
+}
