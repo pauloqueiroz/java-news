d8f531403dbc02756f87aae041ec227651e2bcee
Paulo Sérgio <pauloqueiroz16@gmail.com>
2021-10-10 21:42:41 -0300

    Removendo variáveis desnecessárias

--- src/HttpClientFeatures.java
+++ src/HttpClientFeatures.java
@@ -5,6 +5,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
 
 public class HttpClientFeatures {
 
@@ -16,10 +17,9 @@ public class HttpClientFeatures {
 		System.out.println("Status code: "+response.statusCode());
 		System.out.println("Body: "+response.body());
 		
-		HttpClient client2 = HttpClient.newHttpClient();
-		HttpRequest request2 = HttpRequest.newBuilder(uri).GET().build();
 		System.out.println("------------------Request Async------------------\n");
-		client2.sendAsync(request2, BodyHandlers.ofString()).whenComplete((s, t) -> System.out.println("----------------------------Resposta da request. \nBody: " + s.body()));
+		CompletableFuture<HttpResponse<String>> sendAsync = client.sendAsync(request, BodyHandlers.ofString());
+		sendAsync.whenComplete((s, t) -> System.out.println("----------------------------Resposta da request. \nBody: " + s.body()));
 		System.out.println("\nExecutando Instrucoes seguintes...");
 		System.out.println("\nExecutando outra instrução...");
 		Thread.sleep(10000);
