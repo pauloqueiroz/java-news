16b029be8ec4262e97de3a672587cce28452c112
Paulo Sérgio <pauloqueiroz16@gmail.com>
2021-10-24 19:06:53 -0300

    Criando pool de threads para consumo das notas fiscais

--- src/streams/reactive/NotaFiscalService.java
+++ src/streams/reactive/NotaFiscalService.java
@@ -4,7 +4,8 @@ public class NotaFiscalService {
 
 	public void emitir(NotaFiscal nf) {
 		try {
-			System.out.println("Emitindo nota fiscal.");
+			System.out.println("Emitindo nota fiscal...");
+			System.out.println("Nome da thread: "+Thread.currentThread().getName());
 			Thread.sleep(5000);
 			System.out.format("Nota fiscal de %s emitida com sucesso em %s.\n", nf.getNome(), nf.getData());
 		} catch (InterruptedException e) {
--- src/streams/reactive/TesteReactiveStreams.java
+++ src/streams/reactive/TesteReactiveStreams.java
@@ -2,19 +2,27 @@ package streams.reactive;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 //Java 9
 public class TesteReactiveStreams {
 
 	public static void main(String[] args) throws InterruptedException {
+		ExecutorService threadPool = Executors.newFixedThreadPool(3);
+		System.out.println("Iniciando processamento de notas...");
+		System.out.println("Nome da thread: "+Thread.currentThread().getName());
 		NotaFiscal nota1 = new NotaFiscal("Joao", BigDecimal.valueOf(20), LocalDate.now());
-		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<NotaFiscal>();
+		NotaFiscal nota2 = new NotaFiscal("Pedro", BigDecimal.valueOf(30), LocalDate.now());
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<NotaFiscal>(threadPool,1);
 		NotaFiscalService nfs = new NotaFiscalService();
 		publisher.consume(nfs::emitir);
 		publisher.submit(nota1);
+		publisher.submit(nota2);
 		publisher.close();
 		System.out.println("Processo seguinte a emissão...");
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 	}
 }
