61a878b914ee548f2245f9c6820255c8c8ab22b3
Paulo Sérgio <pauloqueiroz16@gmail.com>
2022-09-25 20:42:12 -0300

    Adicionando implementação de switch de forma reduzida, utilizando múltiplos cases separados por vírgula

--- src/SwitchExpression.java
+++ src/SwitchExpression.java
@@ -1,7 +1,7 @@
 
 public class SwitchExpression {
 
-	//Novidade Java 14 - Switch expressions
+	// Novidade Java 14 - Switch expressions
 	public static void main(String[] args) {
 		String option = "1";
 		switch (option) {
@@ -13,23 +13,39 @@ public class SwitchExpression {
 			break;
 		case "3":
 			System.out.println("opcao 3");
-			break;			
+			break;
 		default:
 			System.out.println("opcao padrao");
 			break;
 		}
-		
+
 		/**
 		 * A partir do Java 14 eh possivel reescrever o switch case acima de forma menos
 		 * verbosa, utilzando lambda expressions, da seguinte forma:
 		 */
-		
+
 		switch (option) {
 		case "1" -> System.out.println("opcao 1");
 		case "2" -> System.out.println("opcao 2");
-		case "3" -> System.out.println("opcao 3");			
+		case "3" -> System.out.println("opcao 3");
 		default -> System.out.println("opcao padrao");
 		}
 		
+		System.out.println(getDiaSemana(DiaSemana.DOMINGO));
+
+	}
+
+	private static String getDiaSemana(DiaSemana dia) {
+		return switch (dia) {
+			case SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA -> "Dia Útil";
+			case SABADO, DOMINGO -> "Fim de Semana";
+			default -> "Dia inválido";
+		
+	
+		};
+	}
+
+	private enum DiaSemana {
+		SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
 	}
 }
